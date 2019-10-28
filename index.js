8e6bafb848e2d6d852dd4e3b38eee76bf5f8b2af
Zai <54860537+zaiscott@users.noreply.github.com>
2019-10-27 21:52:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-var moviePromise = d3.json("https://swapi.co/api/planets");
+var getName = function(planets)
+{
+    return planets.results[0].name
+}
+    
+    
+    
+var displayPlanets = function(planets)
+{
+    d3.select("#names")
+        .selectAll("p")
+        .data(planets)
+        .enter()
+        .append("p")
+        .text(planets.results[0].name)
+             
+}
+
+
+    
+
+var filmPromise = d3.json("https://swapi.co/api/planets")
+    filmPromise.then(
+    function(planets)
+{
+        console.log("planets",planets)
+        displayPlanets(planets)
+        console.log("lol")
+       // console.log(planets.map(getName))
+        
+},
+    function(err)
+{
+    console.log("error")  
+})
 
